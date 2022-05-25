a94a80c48bc6ae93dd504a0f20d4000806165aae
joeytalbot <joeytalbot85@gmail.com>
2022-05-25 16:49:15 +0100

    length

--- code/create-rnet.R
+++ code/create-rnet.R
@@ -19,7 +19,7 @@ rnet_id = stplanr::overline(routes_kildare,
 quietness_breaks = c(0, 25, 50, 75, 100)
 pal = colorspace::sequential_hcl(n = 4, h = c(141, 6), c = c(70, NA, 86), l = c(73, 52), power = c(0.7, 1.9), rev = TRUE)
 
-rnet_id = rnet_id %>%
+rnet_selected = rnet_id %>%
   transmute(
     segment_id = segment_id_first,
     cyclists = round(cyclists_sum),
@@ -31,13 +31,14 @@ rnet_id = rnet_id %>%
     lwd = case_when(cyclists < 20 ~ 20, TRUE ~ cyclists)
   ) %>%
   filter(cyclists > 10) # Remove any rnet segments with small numbers of cyclists
+rnet_selected$length = units::drop_units(st_length(rnet_selected))
 
 # Join the rnet with the street names -------------------------------------
 
 kildare_streets = routes_kildare %>%
   st_drop_geometry() %>%
   select(name, segment_id)
-rnet_join = inner_join(rnet_id, kildare_streets, by = "segment_id")
+rnet_join = inner_join(rnet_selected, kildare_streets, by = "segment_id")
 
 saveRDS(rnet_join, "rnet_kildare_quietest.Rds")
 
