5f0bb7a809254e00ae40660b9567d5fd8bcc7275
joeytalbot <joeytalbot85@gmail.com>
2022-05-25 11:37:20 +0100

    join using segment id

--- code/prioritise.R
+++ code/prioritise.R
@@ -1,14 +1,70 @@
 library(tidyverse)
+library(sf)
+sf_use_s2(FALSE)
+library(stplanr)
 
 # piggyback::pb_download("rnet_combined_quietest.Rds", tag = "0.1")
 # piggyback::pb_download("osm_highways_region.Rds", tag = "0.1")
+# piggyback::pb_download("routes_combined_quietest.Rds", tag = "0.1")
 
 rnet_kildare = readRDS("rnet_combined_quietest.Rds")
+# 20117
 osm_kildare = readRDS("osm_highways_region.Rds")
+routes_kildare = readRDS("routes_combined_quietest.Rds")
+# 404925
 
-# Do roads have a ref? ----------------------------------------------------
+routes_kildare = routes_kildare %>%
+  mutate(segment_id = row_number())
+rnet_id = stplanr::overline(routes_kildare,
+                                  attrib = c("cyclists", "cyclists_10pc", "cyclists_30pc", "quietness", "gradient_smooth", "segment_id"),
+                                  fun = list(sum = sum, mean = mean, first = first)
+)
 
-join_kildare =
+
+quietness_breaks = c(0, 25, 50, 75, 100)
+pal = colorspace::sequential_hcl(n = 4, h = c(141, 6), c = c(70, NA, 86), l = c(73, 52), power = c(0.7, 1.9), rev = TRUE)
+
+rnet_id = rnet_id %>%
+  transmute(
+    segment_id = segment_id_first,
+    `Bicycle (Baseline)` = round(cyclists_sum),
+    `Bicycle (Decarbonise)` = round(cyclists_10pc_sum),
+    `Bicycle (Demand reduction)` = round(cyclists_30pc_sum),
+    Gradient = round(gradient_smooth_mean * 100),
+    Quietness = round(quietness_mean),
+    col = cut(Quietness, quietness_breaks, labels = pal),
+    lwd = case_when(`Bicycle (Baseline)` < 20 ~ 20, TRUE ~ `Bicycle (Baseline)`)
+  ) %>%
+  filter(`Bicycle (Baseline)` > 1)
+
+# Join the rnet with the street names -------------------------------------
+
+kildare_streets = routes_kildare %>%
+  st_drop_geometry() %>%
+  select(name, segment_id)
+rnet_join = inner_join(rnet_id, kildare_streets, by = "segment_id")
+
+
+# Not needed
+# # Many of the route segments are being lost while others are being duplicated during the join process
+# # This would be much more accurate if we could use an ID variable to identify the segments, but I haven't found a way to include a route segment ID in the rnet
+# buff_dist = 10
+# rnet_buff = geo_buffer(shp = rnet_kildare, dist = buff_dist)
+#
+# join_kildare = st_join(routes_kildare, rnet_buff, join = st_within)
+# # 552387
+# join_kildare_filtered = join_kildare %>%
+#   filter(! is.na(Quietness))
+# # 355281
+#
+# kildare_simplified = join_kildare_filtered %>%
+#   select(name, `Bicycle (Baseline)`, `Bicycle (Decarbonise)`, Quietness, col, lwd)
+# system.time({
+#   kildare_unique = distinct(kildare_simplified)
+# })
+
+
+
 
 r_linestrings_with_ref = rnet_kildare %>%
   filter(ref != "")
